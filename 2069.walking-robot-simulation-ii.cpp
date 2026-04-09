b6956f907f33bd6ed9c40eb9293367f1176ba859
ocean <wenocean123@gmail.com>
2026-04-10 01:11:43 +0800

    add 2069 3655

---
+++ 2069.walking-robot-simulation-ii.cpp
@@ -0,0 +1,35 @@
+// @leet imports start
+#include <bits/stdc++.h>
+using namespace std;
+// @leet imports end
+
+// @leet start
+class Robot {
+public:
+    Robot(int width, int height) {
+        
+    }
+    
+    void step(int num) {
+        
+    }
+    
+    vector<int> getPos() {
+        
+    }
+    
+    string getDir() {
+        
+    }
+};
+
+/**
+ * Your Robot object will be instantiated and called as such:
+ * Robot* obj = new Robot(width, height);
+ * obj->step(num);
+ * vector<int> param_2 = obj->getPos();
+ * string param_3 = obj->getDir();
+ */
+// @leet end
+
+int main() {}
---
+++ 3655.xor-after-range-multiplication-queries-ii.cpp
@@ -0,0 +1,15 @@
+// @leet imports start
+#include <bits/stdc++.h>
+using namespace std;
+// @leet imports end
+
+// @leet start
+class Solution {
+public:
+    int xorAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
+        
+    }
+};
+// @leet end
+
+int main() {}
