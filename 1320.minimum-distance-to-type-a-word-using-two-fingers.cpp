89b37910975a7a1919c8813a6724b9241f9d38a0
ocean <wenocean123@gmail.com>
2026-04-12 11:56:40 +0800

    AC 3741

---
+++ 1320.minimum-distance-to-type-a-word-using-two-fingers.cpp
@@ -0,0 +1,15 @@
+// @leet imports start
+#include <bits/stdc++.h>
+using namespace std;
+// @leet imports end
+
+// @leet start
+class Solution {
+public:
+    int minimumDistance(string word) {
+        
+    }
+};
+// @leet end
+
+int main() {}
